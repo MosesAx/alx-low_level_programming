1402ac8eff9581d1334535623e79dd735706ddf6
Moses <kingpinm55@gmail.com>
2022-12-15 15:10:18 -0800

    Task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -4,7 +4,7 @@
  * main - print the number from 1 to 100
  * 3 multiplies print Fizz instead of the number
  * 5 print Buzz instead of number
- * 3 & 5 print FizzBUzz
+ * 3 & 5 print FizzBuzz
  * Return: Always 0
  */
 int main(void)
@@ -18,14 +18,14 @@ int main(void)
 	{
 		if (i == 100)
 			printf("%s", b);
-		else if ((i % 3 == 0) && (i % == 0))
-			printf("%s", fb);
+		else if ((i % 3 == 0) && (i % 5 == 0))
+			printf("%s ", fb);
 		else if (i % 3 == 0)
-			printf("%s", f);
+			printf("%s ", f);
 		else if (i % 5 == 0)
-			printf("%s", b);
+			printf("%s ", b);
 		else
-			printf("%d". i);
+			printf("%d ", i);
 	}
 	printf("\n");
 	return (0);
