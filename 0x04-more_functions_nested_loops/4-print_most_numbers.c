c4c75d02f9978477c309df170578307e2c4a562c
Moses <kingpinm55@gmail.com>
2022-12-15 13:28:15 -0800

    Task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,14 @@
  */
 void print_most_numbers(void)
 {
-	int i;
-	for (i = o; i <= 9; i++)
-	{
-		if ((i == 2) || (i == 4))
-			continue;
+int i;
+for (i = o; i <= 9; i++)
+{
+if ((i == 2) || (i == 4))
+continue;
 
-		else
-			_putchar(i + '0');
-	}
-	_putchar('\n');
+else
+_putchar(i + '0');
+}
+_putchar('\n');
 }
