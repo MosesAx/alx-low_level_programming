df35774fa8b6e5ec521a93239ea3ff648ac00d4f
Moses <kingpinm55@gmail.com>
2023-01-03 10:58:54 -0800

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -4,7 +4,6 @@
  */
 
 #include "main.h"
-
 /**
  * _memcpy - Copies @n bytes from the memory area pointed
  * to by @src into that pointed to by @dest.
@@ -14,6 +13,7 @@
  *
  * Return: A pointer to the destination buffer @dest.
  */
+
 void *_memcpy(void *dest, const void *src, size_t n)
 {
 	unsigned int index;
