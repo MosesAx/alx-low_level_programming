4bad1fbb355d05065b35f9fea8464bb26b15eb06
Moses <kingpinm55@gmail.com>
2023-01-03 11:08:12 -0800

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,11 +1,6 @@
-/*
- * File: 1-memcpy.c
- * Auth: Moses
- */
-
 #include "main.h"
 /**
- * _memcpy - Copies @n bytes from the memory area pointed
+ * *_memcpy - Copies @n bytes from the memory area pointed
  * to by @src into that pointed to by @dest.
  * @dest: A pointer to the memory area to copy @src into.
  * @src: The source buffer to copy characters from.
