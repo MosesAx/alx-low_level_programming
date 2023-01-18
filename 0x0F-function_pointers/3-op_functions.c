0d1ef957724caee8bce15ef65145cc74f393eaa9
Moses <kingpinm55@gmail.com>
2023-01-18 10:41:11 -0800

    Task 3

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -7,7 +7,7 @@ int op_div(int a, int b);
 int op_mod(int a, int b);
 
 /**
- * op_add - Returns the sum of two numbers
+ * op_add - Returns the sum of two numbers.
  * @a: The first number.
  * @b: The second number.
  *
@@ -18,7 +18,7 @@ int op_add(int a, int b)
 	return (a + b);
 }
 /**
- * op_mul - Returns the product of two numbers
+ * op_mul - Returns the product of two numbers.
  * @a: The first number.
  * @b: The second number.
  *
@@ -40,7 +40,7 @@ int op_div(int a, int b)
 	return (a / b);
 }
 /**
- * op_mod - Returns the remainder of the division of two numbers
+ * op_mod - Returns the remainder of the division of two numbers.
  * @a: The first number.
  * @b: The second number.
  *
